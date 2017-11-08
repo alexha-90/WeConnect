dd81e5a15bc8152fa8bb77fcf61a7049c38c78f9
Alex Ha <alexh.90@live.com>
2017-11-07 23:58:20 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Landing from './components/Landing';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Landing/>
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Landing.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Handy Helper landing page placeholder</h1>
+}
+                <Button bsStyle="info">
+                    I need help with task(s)
+                </Button>
+
+                <Button bsStyle="warning">
+                    I can provide help with tasks
+                </Button>
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default App;
