d4883b1fc8add54063e6978f474573d4e38b3b72
alexha <alexh.90@live.com>
2017-12-31 03:32:41 -0800

    Landing page ge plus styling. Fix header responsive

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Button, Grid, Row, Col, Jumbotron } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 //import { Redirect } from 'react-router';
 //===============================================================================================//
@@ -8,21 +8,32 @@ class Landing extends Component {
 
     render() {
         return (
-            <div>
-                <div style={{textAlign: 'center'}}>
-                    <h1>SocialConnector landing page placeholder</h1>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button bsStyle="info">
-                        <Link to="ContentCreatorsList">
-                            Content creators looking for revenue
-                        </Link>
-                    </Button>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Button bsStyle="warning">
-                        <Link to="AdvertisersList">
-                            Advertisers looking to promote
-                        </Link>
-                    </Button>
+            <div className="landing">
+                <div className="mainSplash">
+                    <Grid id="container">
+                        <Row>
+                            <Col sm={8} md={7}>
+                                <Jumbotron id="jumbotron">
+                                    <h1>Engage with the world.</h1>
+                                    <p>
+                                        List your active social platforms and rough metrics. Advertisers will reach
+                                        out to you if they are interested in partnering. Leverage our platform to reach
+                                        out to influential content creators and expand your market presence. Promote a new product.
+                                    </p>
+                                    <br/>
+                                    <Button bsStyle="primary">Learn more</Button>
+                                    &nbsp;
+                                    <Button bsStyle="info">Browse content creators</Button>
+                                    &nbsp;
+                                    <Button bsStyle="success">Register</Button>
+                                </Jumbotron>
+                            </Col>
+                            <Col sm={2} md={1} />
+                            <Col sm={2} md={4}>
+                                <img src="https://i.imgur.com/n6INZhE.png" alt="magnetSocial" />
+                            </Col>
+                        </Row>
+                    </Grid>
                 </div>
             </div>
         )
