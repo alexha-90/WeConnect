2fea59c85f309ade582b924f06ff7d2747e5d564
alexha <alexh.90@live.com>
2018-01-03 04:09:56 -0800

    Add deploy rule in client package.json

--- index.js
+++ index.js
@@ -27,34 +27,34 @@ app.use(bodyParser.urlencoded({ extended: true })); // handle URL-encoded data
 
 
 // session secret. See https://github.com/expressjs/session#options . May need some adjusting later
-// const session = require('express-session');
-// const pgSession = require('connect-pg-simple')(session);
-// app.use(session({
-//     store: new pgSession({
-//         conString: process.env.PGHOST
-//     }),
-//     secret: 'ooeortkoksdfisij',  //process.env.SECRET_KEY
-//     resave: false, // review
-//     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-//     saveUninitialized: false
-//     // for HTTPS cookie: { secure: true }
-// }));
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+app.use(session({
+    store: new pgSession({
+        conString: process.env.PGHOST
+    }),
+    secret: 'ooeortkoksdfisij',  //process.env.SECRET_KEY
+    resave: false, // review
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    saveUninitialized: false
+    // for HTTPS cookie: { secure: true }
+}));
 
 
 // user authentication middleware
-// const passport = require('passport');
-// app.use(passport.initialize());
-// app.use(passport.session());
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // import routes
 //===========================================================================
-// require('./routes/createContentPost')(app);
-// require('./routes/readContentPosts')(app);
-// require('./routes/updateContentPost')(app);
-// require('./routes/deleteContentPost')(app);
-// require('./routes/authRoutes')(app);
-// require('./routes/privateMessages')(app);
+require('./routes/createContentPost')(app);
+require('./routes/readContentPosts')(app);
+require('./routes/updateContentPost')(app);
+require('./routes/deleteContentPost')(app);
+require('./routes/authRoutes')(app);
+require('./routes/privateMessages')(app);
 
 
 // basic test routes
@@ -76,16 +76,6 @@ app.get('/test', (req, res) => {
 //app.use(express.static(path.resolve(__dirname, 'client/public')));
 //app.get('*', (req,res) => res.sendFile(path.join(__dirname, 'client', 'public', 'index.html')));
 //app.use(express.static('client/dist'));
-
-// if (process.env.NODE_ENV === 'production') {
-//     console.log('******** in production environment ********');
-//
-//     app.use(express.static(path.resolve(__dirname, 'client/build')));
-//
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
 if (process.env.NODE_ENV === 'production') {
     console.log('******** in production environment ********');
 
